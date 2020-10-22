7b9d7b7021a768f938025a1834fa7de120dc5964
Mohan <uk_ci@yahoo.com>
2020-10-22 15:44:03 +0530

    added options

--- python/date_add.py
+++ python/date_add.py
@@ -1,7 +1,35 @@
 import datetime
+import argparse
+import sys
 
-startDate = datetime.date(2020,8,17)
-delta = datetime.timedelta(days=270)
+dateObj=datetime.date.today()
+today=f'{dateObj.day}/{dateObj.month}/{dateObj.year}'
+
+argParser = argparse.ArgumentParser(description="Caclulate date on delta days")
+argParser.add_argument('-s','--start_date',type=str,required=False,default=today,help="Start Date (dd/mm/YYYY)")
+argParser.add_argument('-d','--delta',type=int,required=True,help="Day(s) delta (10)")
+
+parsedArgs = vars(argParser.parse_args())
+
+try:
+	parsedArgs['start_date'].index('/')
+	[d,m,y] = parsedArgs['start_date'].split('/')
+	d=int(d)
+	m=int(m)
+	y=int(y)
+except ValueError as e:
+	argParser.print_help()
+	sys.exit(-1)
+
+
+try:
+	startDate = datetime.date(year=y,month=m,day=d)
+except ValueError as e:
+	print(e)
+	argParser.print_help()
+	sys.exit(-1)
+
+delta = datetime.timedelta(days=parsedArgs['delta'])
 
 endDate = startDate + delta
 
